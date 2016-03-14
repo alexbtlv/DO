3b97b5d7b3eaa5f3af8e1f6b94ab737031021fd1
alex <alexbtlv@gmail.com>
2016-03-14 21:51:19 +0500

    added empty OverviewVC

---
+++ DO/OverviewViewController.swift
@@ -0,0 +1,42 @@
+//
+//  OverviewViewController.swift
+//  DO
+//
+//  Created by Alexander Batalov on 3/14/16.
+//  Copyright © 2016 Alexander Batalov. All rights reserved.
+//
+
+import UIKit
+
+class OverviewViewController: UIViewController {
+
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        self.revealViewController().rearViewRevealWidth = view.frame.width - Constants.swRevealFrontViewControllerWidth
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
